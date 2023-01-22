59c2bcc104233c67bd72560e0c97434b926d3110
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-22 21:18:21 +0200

    Document component has been created

---
+++ src/Document.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const contentStyle = {
+  width: "50vw",
+  height: "30vh",
+  overflowX: "hidden",
+  overflowY: "scroll",
+};
+
+const Document = ({ title, content }) => {
+    //Activity state of the button
+    const [disable, setDisable] = useState(false);
+
+  const handleScroll = (e) => {
+      const scrollHeight = e.target.scrollHeight;
+      const offsetHeight = e.target.offsetHeight;
+      const difference = scrollHeight - offsetHeight;
+
+      //Position to the bottom
+      const posToBottom = difference - (e.target.scrollTop + 1); 
+      console.log(posToBottom);
+
+      if (posToBottom === 0) {
+        setDisable(false);
+      } else {
+        setDisable(true);
+      }
+  };
+
+  return (
+    <section>
+      <h2 className="title">{title}</h2>
+      <article
+        className="content"
+        style={contentStyle}
+        onScroll={(e) => handleScroll(e)}
+      >
+        {content}
+      </article>
+      <button disabled={disable}>I Agree</button>
+    </section>
+  );
+};
+
+export default Document;
